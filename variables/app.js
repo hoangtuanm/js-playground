0834dacfa6c58b8867e7c21f585a5b16d1f5b7b6
Tuan <v-tuhoan@microsoft.com>
2018-01-26 12:19:24 -0800

    Initialized Project: Todo

---
+++ variables/app.js
@@ -0,0 +1,26 @@
+// // var, let , const
+
+// var name = 'John Doe';
+// console.log(name);
+
+// name = "Steve Smith";
+// console.log(name);
+
+// // Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// // letter, numbers, _, $
+// // Can not start with number
+// var name = 'test'
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+val = num1 % num2;
+
+console.log(num2);
+
