87dcdbd07bee1ed094be64a6df1a3f84c0c11909
Tuan <v-tuhoan@microsoft.com>
2018-01-26 15:04:32 -0800

    Added Delete and Filter feature

--- project-todo/app.js
+++ project-todo/app.js
@@ -13,6 +13,13 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear all tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks events
+  filter.addEventListener('keyup', filterTasks);
+
 }
 
 function addTask(e) {
@@ -42,4 +49,36 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are your sure?'))
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+// Clear all Tasks
+function clearTasks() {
+  //taskList.innerHTML = '';
+
+  // Faster https://jsperf.com/innerhtml-vs-removechild
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
