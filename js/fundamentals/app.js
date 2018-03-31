18be281a20e01bc60a23ad572573b74b3a22f79a
Tuan Hoang <hoangtuanm@Tuans-MacBook-Pro.local>
2018-03-31 11:58:05 -0700

    JS - Output to console

---
+++ js/fundamentals/app.js
@@ -0,0 +1,28 @@
+// Log to conosle
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1, 2, 3, 4]);
+console.log({a: 1, b:2});
+console.table({a: 1, b: 2});
+
+console.warn('This is some warning.');
+console.error('This is some error.');
+
+console.time('thisValueAsIdentifier');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('thisValueAsIdentifier');
+
+/*
+  multi
+  line
+  comments
+*/
